fa3524535528f497f5ab9795b9aba2018233a547
zenithy <wogwugwu.zenith@gmail.com>
2019-10-19 23:09:07 +0100

    Another update

--- src/app/movies/movie-thumbnail.component.ts
+++ src/app/movies/movie-thumbnail.component.ts
@@ -6,15 +6,26 @@ import {Component, Input} from '@angular/core';
     templateUrl: './movie-thumbnail.component.html',
     styles: [
         `article {
-            padding:.6rem;
             margin-bottom:2rem;
-            margin-right:.3rem;
-            height:17rem;
+            margin-right:.5rem;
+            height:27rem;
             background-color:blue;
             color:#fff;
             float: left;
             width: 20%;
-        }`
+        }
+        .content-div{
+            padding:.6rem;
+        }
+        .img-div{
+            height:10rem;
+            overflow:hidden;
+            background-color:#000;
+        }
+        img{
+            width:100%;
+        }
+        `
     ]
 })
 
--- src/app/movies/movies-list.component.ts
+++ src/app/movies/movies-list.component.ts
@@ -10,10 +10,20 @@ import moviesList from 'src/assets/movies';
             display: table;
             clear: both;
           }
+          input{
+              width:100%;
+              margin-bottom:.6rem;
+          }
         `
     ]
 })
 
 export class MovieListComponent {
     movies = moviesList;
+
+    searchMoviesList (event: KeyboardEvent) {
+        const searchTerm =  (event.target as HTMLInputElement).value;
+        
+        this.movies = moviesList.filter(movie => movie.Title.substring(0, searchTerm.length).toLowerCase() === searchTerm.toLowerCase());
+    }
 }
